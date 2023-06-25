a583a7317d372b889a696d3ea83061af85030ca3
Kamoliddin <kamoliddinxudoyberdiyev72@gmail.com>
2023-06-25 13:59:23 +0500

    hbs

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const config = require('config');
+const exHbs = require('express-handlebars');
+const app = express();
+
+
+const hbs = exHbs.create({
+    defaultLayout: "main",
+    extname: "hbs"
+});
+app.engine("hbs", hbs.engine);
+app.set("View engine", "hbs");
+app.set("views", "views")
+app.use(express.static("views"));
+
+
+const start = async () => {
+    try {
+        const PORT = config.get("port") || 3030;
+        app.listen(PORT, () => {
+            console.log(`Server is running on port: ${PORT}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+start();
