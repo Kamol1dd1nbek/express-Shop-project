ee32fce5b8fe016c48916f6def7609e24d2f10ea
Kamoliddin <kamoliddinxudoyberdiyev72@gmail.com>
2023-06-25 17:43:02 +0500

    Login

--- controller/login.controller.js
+++ controller/login.controller.js
@@ -1,3 +1,5 @@
+const User = require("../models/User.model");
+const bcrypt = require('bcrypt');
 const getLogin = (req, res) => {
     res.render("login", {
         title: "LogIn | Book Shop",
@@ -5,8 +7,26 @@ const getLogin = (req, res) => {
     });
 }
 
-const postLogin = (req, res) => {
-    console.log(req.body);
+const postLogin = async (req, res) => {
+    const {
+        email,
+        password
+    } = req.body;
+
+    const existUser = await User.findOne({email});
+
+    if (!existUser) {
+        console.log("User not found");
+        return;
+    }
+
+    const isPasswordEqual = await bcrypt.compare(password, existUser.password);
+
+    if (!isPasswordEqual) {
+        console.log("Email yoki parol noto'g'ri");
+        return;
+    }
+    console.log(existUser);
     res.redirect("/");
 }
 
