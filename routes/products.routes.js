564e6e03a17c97e879aabdc90d55dc3495739754
Kamoliddin <kamoliddinxudoyberdiyev72@gmail.com>
2023-06-26 00:28:01 +0500

    main page

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -2,11 +2,14 @@ const { Router } = require("express");
 const { addProduct, add } = require("../controller/add.controller");
 const userMiddleware = require("../middleware/user.middleware")
 const authMiddleware = require("../middleware/auth");
+const Product = require("../models/Product.model");
 const router = Router();
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+    const products = await Product.find({}).lean();
     res.render("index", {
         title: "Main Page | Book Shop",
+        products
     });
 });
 router.get("/products", (req, res) => {
