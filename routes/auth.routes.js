7a29f1534c81b62751ad7b07f466e832b41ae8ac
Kamoliddin <kamoliddinxudoyberdiyev72@gmail.com>
2023-06-25 15:23:19 +0500

    Dynamic title

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,12 +3,14 @@ const router = Router();
 
 router.get("/login", (req, res) => {
     res.render("login", {
-        title: "LogIn | Book Shop"
+        title: "LogIn | Book Shop",
+        isLogin: true
     });
 });
 router.get("/register", (req, res) => {
     res.render("register", {
-        title: "Register | Book Shop"
+        title: "Register | Book Shop",
+        isRegister: true
     });
 });
 
